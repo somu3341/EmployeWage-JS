dd7b7b51b0bd0fcc5b85be1c8b3d5a371d8322ea
somu3341 <somashekarvsv1996@gmail.com>
2023-04-26 20:06:56 +0530

    [somu]Add:Calculate Wages Till Condition Working Hour(20) or Day(20).

--- EmployeeWageJS.js
+++ EmployeeWageJS.js
@@ -70,3 +70,15 @@ for(let day=0; day < NUMOF_WORKING_HRS; day++){
 }
 empWage=empHRS * WAGE_PER_HR;
 console.log("Total Hrs: "+empHRS+" Emp Wage: "+empWage)
+//UC5
+const MAX_HRS_IN_MONTH =160;
+const NUM_OF_WORKING_DAYS=20;
+let totalEmpHrs = 0;
+let totalWorkingDays=0;
+while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
+    let empCheck =Math.floor(Math.random() * 10)%3
+    totalEmpHrs += getWorkingHours(empCheck);
+}
+empWage=totalEmpHrs * WAGE_PER_HR;
+console.log("UC5 - Total Days: " + totalWorkingDays + " Total Hrs: "+totalEmpHrs+" Emp Wage: "+empWage);
