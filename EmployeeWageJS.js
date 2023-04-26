925ae7a5bd5b68e9398a06dbcbeb8ca181393081
somu3341 <somashekarvsv1996@gmail.com>
2023-04-26 17:26:05 +0530

    [somu]Add:Calculating Employee Wage By PartTime or FullTime.

--- EmployeeWageJS.js
+++ EmployeeWageJS.js
@@ -8,3 +8,19 @@ if(empCheck == IS_ABSENT){
 else{
     console.log("Employee is PRESENT");
 }
+//UC2
+const FULL_TIME=0, PART_TIME=1,ABSENT=2,PART_TIME_HR=4,FULL_TIME_HR=8,WAGE_PER_HR=20;
+let empHR=0;
+checkemp=Math.floor(Math.random()*3);
+switch(checkemp){
+    case FULL_TIME:
+        checkemp=PART_TIME_HR;
+        break;
+        case PART_TIME:
+            checkemp=FULL_TIME_HR;
+            break;
+            case ABSENT:
+                checkemp=empHR;                
+}
+let empWage=checkemp*WAGE_PER_HR;
+console.log("Employee Wage = "+empWage);
