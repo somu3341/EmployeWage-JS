09449ca2a27c10d5d216a8aa048e554363ef383a
somu3341 <somashekarvsv1996@gmail.com>
2023-04-30 19:59:17 +0530

    [somu]Add:Cal Total Wage and Total Hours Worked, Working Days Using Arrow Functions.

--- EmployeeWageJS.js
+++ EmployeeWageJS.js
@@ -162,3 +162,22 @@ function totalWages(totalWage, dailyWage){
     return totalWage + dailyWage;
 }
 console.log("UC8-Emp Map totalHrs: "+Array.from(empDailyWageMap.values()).reduce(totEmpWage, 0));
+//UC9
+const findTotal=(totalVal, dailyVal) => {
+    return  totalVal + dailyVal;
+}
+let count=0;
+let totalHours = Array.from(empDailyHrsMap.values()).reduce(findTotal, 0);
+let totalSalary = empDailyWageArr.filter(dailyWage => dailyWage > 0).reduce(findTotal, 0);
+console.log("UC9A - Emp Wage with Arrow: "+" Total Hours: "+totalHours+" Total Wages: "+totalSalary);
+let nonWorkingDays = new Array();
+let partWorkingDays = new Array();
+let fullWorkingDays = new Array();
+empDailyHrsMap.forEach( (value, key, map) => {
+    if (value == 8) fullWorkingDays.push(key);
+    else if (value == 4) partWorkingDays.push(key);
+    else nonWorkingDays.push(key);
+});
+console.log("Full Working Days: "+ fullWorkingDays);
+console.log("Part Time Working Days: "+partWorkingDays);
+console.log("Non Working Days: "+nonWorkingDays);
